32fa15ad3f475d8d62065b191284b6dd822c6137
Saurabh kesharwani <saurabh29111999@gmail.com>
2022-01-03 12:32:13 +0530

    first commit

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -5,7 +5,7 @@ const Navbar = () => {
   return (
     <div className="col-md-12 bg-dark py-2">
       <nav className="navbar bg-dark navbar-dark">
-        <Link to={"/"} className="navbar-brand ml-5 text-center">
+        <Link to={"/"} className="container-fluid navbar-brand">
           List of Contact Book
         </Link>
       </nav>
